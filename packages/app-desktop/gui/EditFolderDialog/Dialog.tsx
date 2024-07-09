6f334f0e6d74805007ae87457ab980577691a298
Naresh <nareshkumarthodupunoori@gmail.com>
2024-07-09 23:41:03 +0530

    Subtitle column is added to Create Notebook Section

--- packages/app-desktop/gui/EditFolderDialog/Dialog.tsx
+++ packages/app-desktop/gui/EditFolderDialog/Dialog.tsx
@@ -1,184 +1,210 @@
-import * as React from 'react';
-import { useCallback, useState, useRef, useEffect, useId } from 'react';
-import { _ } from '@joplin/lib/locale';
-import DialogButtonRow, { ClickEvent } from '../DialogButtonRow';
-import Dialog from '../Dialog';
-import DialogTitle from '../DialogTitle';
-import StyledInput from '../style/StyledInput';
-import { IconSelector, ChangeEvent } from './IconSelector';
-import useAsyncEffect, { AsyncEffectEvent } from '@joplin/lib/hooks/useAsyncEffect';
-import Folder from '@joplin/lib/models/Folder';
-import { FolderEntity, FolderIcon, FolderIconType } from '@joplin/lib/services/database/types';
-import Button from '../Button/Button';
-import bridge from '../../services/bridge';
-import shim from '@joplin/lib/shim';
-import FolderIconBox from '../FolderIconBox';
-import { focus } from '@joplin/lib/utils/focusHandler';
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value: unknown): Promise<unknown> {
+        return value instanceof Promise ? value : new Promise(resolve => resolve(value));
+    }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value: unknown) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value: unknown) { try { step(generator.throw(value)); } catch (e) { reject(e); } }
+        function step(result: IteratorResult<unknown>) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 
-interface Props {
-	themeId: number;
-	// eslint-disable-next-line @typescript-eslint/ban-types -- Old code before rule was applied
-	dispatch: Function;
-	folderId: string;
-	parentId: string;
+import React, { useState, useRef, useCallback, useEffect } from "react";
+import { _ } from "@joplin/lib/locale";
+import DialogButtonRow from "../DialogButtonRow";
+import Dialog from "../Dialog";
+import DialogTitle from "../DialogTitle";
+import StyledInput from "../style/StyledInput";
+import { IconSelector } from "./IconSelector";
+import useAsyncEffect from "@joplin/lib/hooks/useAsyncEffect";
+import Folder from "@joplin/lib/models/Folder";
+import { FolderIconType } from "@joplin/lib/services/database/types";
+import Button from "../Button/Button";
+import bridge from "../../services/bridge";
+import shim from "@joplin/lib/shim";
+import FolderIconBox from "../FolderIconBox";
+import { focus } from "@joplin/lib/utils/focusHandler";
+
+interface DialogProps {
+    folderId?: string;
+    parentId?: string;
+    dispatch: Function; // This could be further typed if you have a specific type for dispatch
+    themeId: number;
 }
 
-export default function(props: Props) {
-	const [folderTitle, setFolderTitle] = useState('');
-	const [folderIcon, setFolderIcon] = useState<FolderIcon>();
-	const titleInputRef = useRef(null);
+function DialogComponent(props: DialogProps) {
+    const [folderTitle, setFolderTitle] = useState('');
+    const [foldersubtitle, setFoldersubtitle] = useState('');
+    const [folderIcon, setFolderIcon] = useState<any>();
+    const titleInputRef = useRef<HTMLInputElement>(null);
+    const subtitleInputRef = useRef<HTMLInputElement>(null);
+    const isNew = !props.folderId;
 
-	const isNew = !props.folderId;
+    useAsyncEffect((event) => __awaiter(this, void 0, void 0, function* () {
+        if (isNew) return;
+        const folder = yield Folder.load(props.folderId);
+        if (event.cancelled) return;
+        setFolderTitle(folder.title);
+        setFoldersubtitle(folder.subtitle);
+        setFolderIcon(Folder.unserializeIcon(folder.icon));
+    }), [props.folderId, isNew]);
 
-	useAsyncEffect(async (event: AsyncEffectEvent) => {
-		if (isNew) return;
+    const onClose = useCallback(() => {
+        props.dispatch({
+            type: 'DIALOG_CLOSE',
+            name: 'editFolder',
+        });
+    }, [props.dispatch]);
 
-		const folder = await Folder.load(props.folderId);
-		if (event.cancelled) return;
-		setFolderTitle(folder.title);
-		setFolderIcon(Folder.unserializeIcon(folder.icon));
-	}, [props.folderId, isNew]);
+    useEffect(() => {
+        focus('Dialog::titleInputRef', titleInputRef.current);
+        setTimeout(() => {
+            titleInputRef.current.select();
+        }, 100);
+    }, []);
 
-	const onClose = useCallback(() => {
-		props.dispatch({
-			type: 'DIALOG_CLOSE',
-			name: 'editFolder',
-		});
-	}, [props.dispatch]);
+    useEffect(() => {
+        focus('Dialog::subtitleInputRef', subtitleInputRef.current);
+        setTimeout(() => {
+            subtitleInputRef.current.select();
+        }, 100);
+    }, []);
 
-	useEffect(() => {
-		focus('Dialog::titleInputRef', titleInputRef.current);
+    const onButtonRowClick = useCallback(async (event: { buttonName: string }) => {
+        if (event.buttonName === 'cancel') {
+            onClose();
+            return;
+        }
+        if (event.buttonName === 'ok') {
+            const folder = {
+                title: folderTitle,
+                subtitle: foldersubtitle,
+                icon: Folder.serializeIcon(folderIcon),
+            };
+            if (!isNew) folder.id = props.folderId;
+            if (props.parentId) folder.parent_id = props.parentId;
+            try {
+                const savedFolder = await Folder.save(folder, { userSideValidation: true });
+                onClose();
+                props.dispatch({
+                    type: 'FOLDER_SELECT',
+                    id: savedFolder.id,
+                });
+            } catch (error) {
+                bridge().showErrorMessageBox(error.message);
+            }
+            return;
+        }
+    }, [onClose, folderTitle, foldersubtitle, folderIcon, props.folderId, props.parentId, props.dispatch]);
 
-		setTimeout(() => {
-			titleInputRef.current.select();
-		}, 100);
-	}, []);
+    const onFolderTitleChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
+        setFolderTitle(event.target.value);
+    }, []);
 
-	const onButtonRowClick = useCallback(async (event: ClickEvent) => {
-		if (event.buttonName === 'cancel') {
-			onClose();
-			return;
-		}
+    const onFoldersubtitleChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
+        setFoldersubtitle(event.target.value);
+    }, []);
 
-		if (event.buttonName === 'ok') {
-			const folder: FolderEntity = {
-				title: folderTitle,
-				icon: Folder.serializeIcon(folderIcon),
-			};
+    const onFolderIconChange = useCallback((event: { value: any }) => {
+        setFolderIcon(event.value);
+    }, []);
 
-			if (!isNew) folder.id = props.folderId;
-			if (props.parentId) folder.parent_id = props.parentId;
+    const onClearClick = useCallback(() => {
+        setFolderIcon(null);
+    }, []);
 
-			try {
-				const savedFolder = await Folder.save(folder, { userSideValidation: true });
-				onClose();
+    const onBrowseClick = useCallback(async () => {
+        const filePaths = await bridge().showOpenDialog({
+            filters: [
+                {
+                    name: _('Images'),
+                    extensions: ['jpg', 'jpeg', 'png'],
+                },
+            ],
+        });
+        if (filePaths.length !== 1) return;
+        const filePath = filePaths[0];
+        try {
+            const dataUrl = await shim.imageToDataUrl(filePath, 256);
+            setFolderIcon(icon => {
+                return { ...icon, emoji: '', name: '', type: FolderIconType.DataUrl, dataUrl };
+            });
+        } catch (error) {
+            await bridge().showErrorMessageBox(error.message);
+        }
+    }, []);
 
-				props.dispatch({
-					type: 'FOLDER_SELECT',
-					id: savedFolder.id,
-				});
-			} catch (error) {
-				bridge().showErrorMessageBox(error.message);
-			}
+    const formTitleInputId = useId();
+    const formsubtitleInputId = useId();
 
-			return;
-		}
-		// eslint-disable-next-line @seiyab/react-hooks/exhaustive-deps -- Old code before rule was applied
-	}, [onClose, folderTitle, folderIcon, props.folderId, props.parentId]);
+    function renderForm() {
+        return (
+            <div>
+                <div className="form">
+                    <div className="form-input-group">
+                        <label htmlFor={formTitleInputId}>{_('Title')}</label>
+                        <StyledInput
+                            id={formTitleInputId}
+                            type="text"
+                            ref={titleInputRef}
+                            value={folderTitle}
+                            onChange={onFolderTitleChange}
+                        />
+                    </div>
+                    <div className="form-input-group">
+                        <label htmlFor={formsubtitleInputId}>{_('subtitle')}</label>
+                        <StyledInput
+                            id={formsubtitleInputId}
+                            type="text"
+                            ref={subtitleInputRef}
+                            value={foldersubtitle}
+                            onChange={onFoldersubtitleChange}
+                        />
+                    </div>
+                    <div className="form-input-group">
+                        <label>{_('Icon')}</label>
+                        <div className="icon-selector-row">
+                            {folderIcon && (
+                                <div className="foldericon">
+                                    <FolderIconBox folderIcon={folderIcon} />
+                                </div>
+                            )}
+                            <IconSelector
+                                title={_('Select emoji...')}
+                                icon={folderIcon}
+                                onChange={onFolderIconChange}
+                            />
+                            <Button ml={1} title={_('Select file...')} onClick={onBrowseClick} />
+                            {folderIcon && <Button ml={1} title={_('Clear')} onClick={onClearClick} />}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any -- Old code before rule was applied
-	const onFolderTitleChange = useCallback((event: any) => {
-		setFolderTitle(event.target.value);
-	}, []);
+    function renderContent() {
+        return <div className="dialog-content">{renderForm()}</div>;
+    }
 
-	const onFolderIconChange = useCallback((event: ChangeEvent) => {
-		setFolderIcon(event.value);
-	}, []);
+    const dialogTitle = isNew ? _('Create notebook') : _('Edit notebook');
+    const dialogsubtitle = _('Fill in the details below');
 
-	const onClearClick = useCallback(() => {
-		setFolderIcon(null);
-	}, []);
+    function renderDialogWrapper() {
+        return (
+            <div className="dialog-root">
+                <DialogTitle title={dialogTitle} />
+                <div className="dialog-subtitle">{dialogsubtitle}</div>
+                {renderContent()}
+                <DialogButtonRow themeId={props.themeId} onClick={onButtonRowClick} />
+            </div>
+        );
+    }
 
-	const onBrowseClick = useCallback(async () => {
-		const filePaths = await bridge().showOpenDialog({
-			filters: [
-				{
-					name: _('Images'),
-					extensions: ['jpg', 'jpeg', 'png'],
-				},
-			],
-		});
-		if (filePaths.length !== 1) return;
-		const filePath = filePaths[0];
-
-		try {
-			const dataUrl = await shim.imageToDataUrl(filePath, 256);
-			setFolderIcon(icon => {
-				return {
-					...icon,
-					emoji: '',
-					name: '',
-					type: FolderIconType.DataUrl,
-					dataUrl,
-				};
-			});
-		} catch (error) {
-			await bridge().showErrorMessageBox(error.message);
-		}
-	}, []);
-
-	const formTitleInputId = useId();
-	function renderForm() {
-		return (
-			<div>
-				<div className="form">
-					<div className="form-input-group">
-						<label htmlFor={formTitleInputId}>{_('Title')}</label>
-						<StyledInput id={formTitleInputId} type="text" ref={titleInputRef} value={folderTitle} onChange={onFolderTitleChange}/>
-					</div>
-
-					<div className="form-input-group">
-						<label>{_('Icon')}</label>
-						<div className="icon-selector-row">
-							{ folderIcon && <div className="foldericon"><FolderIconBox folderIcon={folderIcon} /></div> }
-							<IconSelector
-								title={_('Select emoji...')}
-								icon={folderIcon}
-								onChange={onFolderIconChange}
-							/>
-							<Button ml={1} title={_('Select file...')} onClick={onBrowseClick}/>
-							{ folderIcon && <Button ml={1} title={_('Clear')} onClick={onClearClick}/> }
-						</div>
-					</div>
-				</div>
-			</div>
-		);
-	}
-
-	function renderContent() {
-		return (
-			<div className="dialog-content">
-				{renderForm()}
-			</div>
-		);
-	}
-
-	const dialogTitle = isNew ? _('Create notebook') : _('Edit notebook');
-
-	function renderDialogWrapper() {
-		return (
-			<div className="dialog-root">
-				<DialogTitle title={dialogTitle}/>
-				{renderContent()}
-				<DialogButtonRow
-					themeId={props.themeId}
-					onClick={onButtonRowClick}
-				/>
-			</div>
-		);
-	}
-
-	return (
-		<Dialog onClose={onClose} className="master-password-dialog" renderContent={renderDialogWrapper}/>
-	);
+    return (
+        <Dialog onClose={onClose} className="master-password-dialog" renderContent={renderDialogWrapper} />
+    );
 }
+export default DialogComponent;
